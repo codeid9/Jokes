906068667a6ca311ab8fc0ae15ec15dfe9953339
codeid9 <abhaychhani@gmail.com>
2026-02-27 11:30:43 +0530

    cookies option fix

--- Backend/src/controllers/user.controllers.js
+++ Backend/src/controllers/user.controllers.js
@@ -183,6 +183,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
+        sameSite:"none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     };
     return res
